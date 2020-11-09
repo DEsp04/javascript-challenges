856ed91fc15e9bbb9236538883401fdd61c68d53
david espinal <despinal0425@gmail.com>
2020-11-09 16:44:35 -0500

    do not understand add-eventing

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,26 @@
-const addEventing = function (obj) {
-}
+function addEventing (obj) {
+  let events = {};
+
+  obj.on = (eventName, fn) => {
+    if (events[eventName]) {
+      events[eventName].push(fn)
+    } else {
+      events[eventName] = [fn]
+    }
+  }
+  obj.trigger = (eventName, ...args) => {
+    events[eventName].forEach(fn => fn(...args))
+  }
+
+  return obj
+};
+
+let obj = addEventing({ name: 'John', age: 35 });
+
+obj.on('ageChange', function() {
+  console.log('age changed');
+});
+
+obj.trigger('ageChange');
 
 module.exports = addEventing

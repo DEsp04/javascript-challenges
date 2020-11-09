72e669b10e1769189d4525518b007de7d68483bf
david espinal <despinal0425@gmail.com>
2020-11-09 13:17:11 -0500

    completed stack machine calculator

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,38 @@
-const stackMachineCalculator = (instructions) => {
-  return instructions
+const stackMachineCalculator = (expression) => {
+  const stack = [];
+  const expArray = expression.split(' ');
+  let n1;
+  let n2;
+
+  for(let i = 0; i < expArray.length; i++){
+      switch(expArray[i]) {
+          case "+":
+              stack.push(stack.pop() + stack.pop());
+              break;
+          case "-":
+              n1 = stack.pop();
+              n2 = stack.pop();
+              stack.push(n2 - n1);
+              break;
+          case "*":
+              stack.push(stack.pop() * stack.pop());
+              break;
+          case "/":
+              n1 = stack.pop();
+              n2 = stack.pop();
+              stack.push(n2 / n1);
+              break;
+          case "DUP":
+              stack.splice(stack.length, 0, stack[stack.length - 1]);
+              break;
+          case "POP":
+              stack.pop();
+              break;
+          default:
+              stack.push(parseInt(expArray[i]));
+      }
+  }
+  return stack.pop();
 }
 
 module.exports = stackMachineCalculator
